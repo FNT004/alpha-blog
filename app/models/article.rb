81f4277129aa29544109cf0bcb1a7562adac2ef8
FNT004 <fnabi9@gmail.com>
2022-06-12 16:45:23 +0530

    heroku upload errors

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,8 @@
 class Article < ActiveRecord::Base
     
     validates :title, presence: true, length: { minimum: 3, maximum:150 }
-    validates :description, presence: true, length: {minimum:10, maximum:5000}
+    has_rich_text :description
+    
     belongs_to :user
-    has_rich_text :content
+    
 end
